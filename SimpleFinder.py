a8e35e5f3f6722ae004895a21aee038e62f9dec3
Przemysław Wandzilak <przemyslaw.wandzilak@intel.com>
2015-03-08 01:53:24 +0100

    minor fixup for help option output

--- SimpleFinder.py
+++ SimpleFinder.py
@@ -4,27 +4,28 @@ import os
 import re
 
 def debug1(options, args):
-    print("sample use: " + ''' python SimpleFinder.py -f klj  -d "/c/OPENSW/"''')
+    print("sample use: " + '''python SimpleFinder.py -f klj  -d "/c/OPENSW/" args1 args2''')
     # http://stackoverflow.com/questions/6666856
     # in python 2, all of your classes should inherit from object. 
     # If you don't, you end up with "old style classes", which are 
     # always of type classobj, and whose instances are always of type instance
-    print("type(options)\t\tis " + str(type(options)))        # <type 'instance'>
-    print("options.__class__\tis " + str(options.__class__))    # optparse.values
+    print("type(options)\t\tis " + str(type(options)))                     # <type 'instance'>
+    print("options.__class__\tis " + str(options.__class__))               # optparse.values
     print("type(optparse.Values)\tis " + str(type(optparse.Values)))       # <type 'classobj'>
     print("options:\t" + str(options))
     print("vars(options):\t" + str(vars(options)))
     print("args:\t\t" + str(args))
     print("")
-    #print("options.filename:\t" + str(options.filename  ))
+    print("options.filename:\t" + str(options.filename  ))
+    print("options.number +1:\t" + str(options.number + 1))
     #print("options['filename']\t" + str(options['filename'])) #options is not a dic type
 
 def parseOptions():
     parser = OptionParser()
     parser.add_option("-f", "--file", dest="filename",
-                      help="my filename", metavar="FILE")
+                      help="Find pattern in FILE", metavar="FILE")
     parser.add_option("-d", "--dir", dest="directory",
-                      help="my magix dirname", metavar="FILE")
+                      help="Find pattern in all files in DIR", metavar="DIR")
     parser.add_option("-q", "--quiet",
                       action="store_false", dest="verbose", default=True,
                       help="don't print status messages to stdout")
